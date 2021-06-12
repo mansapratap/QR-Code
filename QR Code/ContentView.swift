045f2c152225d14179c915ec33afcb5dc89bc0a1
Mansa Pratap Singh <Mansa Pratap Singh>
2021-06-12 10:19:43 +0530

    Initial Commit

---
+++ QR Code/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  QR Code
+//
+//  Created by Mansa Pratap Singh on 12/06/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ QR Code/QR_CodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  QR_CodeApp.swift
+//  QR Code
+//
+//  Created by Mansa Pratap Singh on 12/06/21.
+//
+
+import SwiftUI
+
+@main
+struct QR_CodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
